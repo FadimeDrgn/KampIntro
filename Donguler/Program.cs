9577eb6916df7543ed9f582bbdc6455bd1bf956a
Fadime Durğun <durgunfadime@gmail.com>
2021-02-15 22:20:59 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            String kurs2 = "Programlamaya başlangıç için temel şartlar";
+            string kurs3 = "Java";
+
+            //array - dizi
+
+            string[] kurslar = new string[] { };
+
+
+            for(int i = 1; i<10; i=i+2)
+            {
+                Console.WriteLine(i);
+            }
+
+           
+        }
+    }
+}
